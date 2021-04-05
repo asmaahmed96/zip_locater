5f6b3a0600dde59b145f6b32cb3bc6be80d9f968
Peter Ramsing <peter.ramsing@xpo.com>
2021-04-05 11:32:51 -0700

    WIP: pass to pair

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
-﻿let request = new XMLHttpRequest()
-request.open("GET", "https://api.zippopotam.us/us/90210")
-request.send()
-request.onload = () => {
-    console.log(request)
-    var body = XMLHttpRequest.response;
-
-
+﻿function getZip() {
+    let zipToCheck = window.prompt('what ur zip, yo?')
+    let request = new XMLHttpRequest()
+    request.open("GET", "https://api.zippopotam.us/us/" + zipToCheck)
+    request.send()
+    request.onload = () => {
+        console.log(request)
+        setZipToDom(request.response)
+    }
 }
+
+function setZipToDom(response) {
+    response = JSON.parse(response);
+    console.log(response);
+    document.getElementById("js-zipCode").innerHTML = response['post code'];
+    document.getElementById("js-state").innerHTML = response.places[0].state;
+    document.getElementById("js-city").innerHTML = response.places[0]['place name'];
+}
+
+
+getZip();
