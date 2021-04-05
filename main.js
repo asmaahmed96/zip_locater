4cd209b06633b66109e0496fbc90dd17eecc96bd
asmaahmed96 <ahmed021@cougars.csusm.edu>
2021-04-05 11:10:10 -0700

    wip: pass to pair

---
+++ main.js
@@ -0,0 +1,9 @@
+﻿let request = new XMLHttpRequest()
+request.open("GET", "https://api.zippopotam.us/us/90210")
+request.send()
+request.onload = () => {
+    console.log(request)
+    var body = XMLHttpRequest.response;
+
+
+}
